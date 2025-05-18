76c54495b30c4cd1b027298d8114293a224c0903
AGKolosov <ak60ru@gmail.com>
2025-05-18 22:53:35 +0300

    Empty commit to create PR

--- src/main/java/org/skypro/skyshop/SkyshopApplication.java
+++ src/main/java/org/skypro/skyshop/SkyshopApplication.java
@@ -2,7 +2,7 @@ package org.skypro.skyshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Empty commit to create PR
 @SpringBootApplication
 public class SkyshopApplication {
 
